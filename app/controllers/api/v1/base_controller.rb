3ee28d1e3706613e24993f6aa6e2e21a074edfaf
Claudia Lujan <verenisse.eyzaguirre@gmail.com>
2022-08-14 18:07:35 -0500

    remove protect_from_forgery

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -1,4 +1,2 @@
 class Api::V1::BaseController < ActionController::API
-  protect_from_forgery with: :null_session
-
 end
--- app/controllers/api/v1/orders_controller.rb
+++ app/controllers/api/v1/orders_controller.rb
@@ -1,5 +1,4 @@
 class Api::V1::OrdersController < ApplicationController
-  protect_from_forgery with: :null_session
 
   def show
     @order = Order.find(params[:id])
