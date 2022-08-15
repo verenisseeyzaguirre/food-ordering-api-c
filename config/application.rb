af4b5ea0ef687f5ea8eb3ef0dedb929bc0f513e0
Claudia Lujan <verenisse.eyzaguirre@gmail.com>
2022-08-15 10:23:50 -0500

    add rack-cors

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,14 @@ module FoodOrderingApi
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.middleware.insert_before ActionDispatch::Static, Rack::Cors do
+      allow do
+          origins '*'
+          resource '*', :headers => :any, :methods => [:get, :post, :options, :patch, :delete]
+      end
+    end
   end
+
+  
 end
